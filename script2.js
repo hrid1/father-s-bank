a7d6e7a2bfb6925ad905fee9d4bc41e20b928cc6
hrid1 <hridoycse1@gmail.com>
2024-01-05 00:32:24 +0600

    Done

--- script2.js
+++ script2.js
@@ -4,20 +4,46 @@ let depositeMoney = document.getElementById('deposite-amt') ;
 //console.log(depositeMoney);
 
 let withdrawMoney = document.getElementById('withdraw-amt');
-console.log(withdrawMoney);
+//console.log(withdrawMoney);
 
 let balanceMoney = document.getElementById('balance-amt');
-console.log(balanceMoney);
+//console.log(balanceMoney);
+
+
+//----------------------deposite money-----------------------
 
 document.getElementById('deposite-btn').addEventListener('click', function(){
     const depositeField = document.getElementById('deposite-bal');
     const depositeAmount = depositeField.value;
-    console.log(depositeAmount);
-    let money = depositeMoney.innerText+depositeAmount;
-    console.log(money);
+
+    let money = parseInt(depositeMoney.innerText)+parseInt(depositeAmount);
+   
     depositeField.value = '';
     depositeMoney.innerText = money;
-    
-
+    let totalBalance = parseInt(balanceMoney.innerText)+parseInt(depositeAmount);
+    balanceMoney.innerText = totalBalance;
 
 })
+
+// ----------------withdraw money-----------------------------
+
+document.getElementById('withdraw-btn').addEventListener('click',
+function(){
+    const withdrawField = document.getElementById('withdraw-bal');
+    let withdrawAmount = parseInt(withdrawField.value);
+    let totalBalNumber = parseInt(balanceMoney.innerText);; 
+    let totalWithdraw = parseInt(withdrawMoney.innerText);
+
+    if(totalBalNumber > withdrawAmount){
+        totalBalNumber -= withdrawAmount;
+        balanceMoney.innerText = totalBalNumber;
+       
+        totalWithdraw += withdrawAmount;
+        withdrawMoney.innerText = totalWithdraw;
+    }
+    else{
+        alert("sorry!!! ")
+    }
+
+})
+
